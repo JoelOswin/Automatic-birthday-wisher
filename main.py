61f0f742ccf622d8363d03766bea4e69d8fc111f
Joel Oswin J <joeloswin41@gmail.com>
2025-06-25 11:07:07 +0530

    Fixed cron without internet errror

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from app.message_sender import matched_contacts,send_msg
 import requests
 import time
 
-try:
-    r=requests.get('https://www.google.com/')
+while True:
+    try:
+        r=requests.get('https://www.google.com/')
 
-    creds=authenticate('CLIENT_FILE.json',['https://www.googleapis.com/auth/contacts'])
-    all_contacts=fetch_contacts(creds)
-    contacts=matched_contacts(all_contacts)
+        creds=authenticate('CLIENT_FILE.json',['https://www.googleapis.com/auth/contacts'])
+        all_contacts=fetch_contacts(creds)
+        contacts=matched_contacts(all_contacts)
 
-    for contact in contacts:
-        send_msg(name=contact['name'],phoneNumber=contact['phoneNumber'])
-        print(f"msg sent to {contact['name']}")
-        time.sleep(5)
-    else:
-        time.sleep(60)
+        for contact in contacts:
+            send_msg(name=contact['name'],phoneNumber=contact['phoneNumber'])
+            print(f"msg sent to {contact['name']}")
+            time.sleep(5)
+        else:
+            time.sleep(60)
+        break
 
-except requests.exceptions.ConnectionError:
-    print("No Internet, Trying after 30 seconds")
-    time.sleep(30)
+    except requests.exceptions.ConnectionError:
+        print("No Internet, Trying after 30 seconds")
+        time.sleep(30)
+        continue
